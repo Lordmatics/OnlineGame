07b6b6d372ce05800b8a02d31524bf8837370917
Lordmatics <niallmaple@yahoo.co.uk>
2017-02-06 19:39:57 +0000

    Game GameMode CPP Updated
    Just removed the logout function since that will be done in BP, due to
    the fact it connects to the session logic, similiar to kicked.
    Everything else looks good to go in here otherwise

--- Source/OnlineGame/OnlineGameGameMode.cpp
+++ Source/OnlineGame/OnlineGameGameMode.cpp
@@ -8,10 +8,19 @@
 #include "OnlineGameCharacter.h"
 #include "EngineUtils.h"
 
+// OKAY
+// THIS CLASS
+// ALL VARIABLES USED FROM C++ IN BP
+// FUNCTIONS, DONE IN PURE BP - 
+// Logout
+
+// So this class handles, tracking the changes in player controllers from lobby to game
+// And subsequently changing the Base Character to an actual in game character - for everyone
+
 AOnlineGameGameMode::AOnlineGameGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BlueprintsExtensions/Characters/BP_BaseNiall"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
@@ -30,17 +39,17 @@ void AOnlineGameGameMode::Logout(AController* Exiting)
 {
 	Super::Logout(Exiting);
 
-	AOnlineGamePlayerController* MyPlayerController = Cast<AOnlineGamePlayerController>(Exiting);
-	if (MyPlayerController != nullptr)
-	{
-		UOnlineGameInstance* MyGameInstance = Cast<UOnlineGameInstance>(GetGameInstance());
-		if (MyGameInstance != nullptr)
-		{
-			// Disconnect this player controller 
-			// From the existing session
-			MyGameInstance->DestroySessionCaller(MyPlayerController);
-		}
-	}
+	//AOnlineGamePlayerController* MyPlayerController = Cast<AOnlineGamePlayerController>(Exiting);
+	//if (MyPlayerController != nullptr)
+	//{
+	//	UOnlineGameInstance* MyGameInstance = Cast<UOnlineGameInstance>(GetGameInstance());
+	//	if (MyGameInstance != nullptr)
+	//	{
+	//		// Disconnect this player controller 
+	//		// From the existing session
+	//		MyGameInstance->DestroySessionCaller(MyPlayerController);
+	//	}
+	//}
 }
 
 void AOnlineGameGameMode::ServerRespawnPlayerNetwork_Implementation(APlayerController* _PlayerController, TSubclassOf<ACharacter> _PlayerCharacter)
